3b26a1725f7bbb08aa1b64513332c614b5d1b267
Menglong TAN <tanmenglong@gmail.com>
2015-07-01 14:57:14 +0800

    add baidu style log

---
+++ baidulog.go
@@ -0,0 +1,31 @@
+/***************************************************************
+ *
+ * Copyright (c) 2015, Menglong TAN <tanmenglong@gmail.com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GPL licence
+ *
+ **************************************************************/
+
+/**
+ * Baidu style logger
+ *
+ * @file baidulog.go
+ * @author Menglong TAN <tanmenglong@gmail.com>
+ * @date Wed Jul  1 14:48:23 2015
+ *
+ **/
+
+package gglog
+
+import (
+	"fmt"
+)
+
+//===================================================================
+// Public APIs
+//===================================================================
+
+//===================================================================
+// Private
+//===================================================================
